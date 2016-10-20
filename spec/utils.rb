34c95851def37bf91d7f3ca96c75dae818471b6a
Elliott Williams <e@elliottwillia.ms>
2016-10-20 10:23:16 -0400

    Added output checking and type checking matchers

--- spec/utils.rb
+++ spec/utils.rb
@@ -14,26 +14,50 @@ def parser_accepts? code
   end
 end
 
+def parser_outputs? expected, given:
+  output, _ = Open3.capture2e '../parser', stdin_data: given
+  output == expected
+end
+
+def parser_typechecks? code
+  output, _ = Open3.capture2e '../parser', stdin_data: code
+  if /type violation$/ =~ output.lines.first
+    false
+  else
+    true
+  end
+end
+
+def enclose_statements code
+  <<-EOS
+  <script type="text/JavaScript">
+  #{code}
+  </script>
+  EOS
+end
+
 module RSpec
   Matchers.define :accept do
-    match do |code|
-      parser_accepts? code
-    end
+    match { |code| parser_accepts? code }
   end
   Matchers.alias_matcher :accepted, :accept
   Matchers.define_negated_matcher :reject, :accept
   Matchers.alias_matcher :rejected, :reject
 
   Matchers.define :be_a_statement do
-    match do |code|
-      parser_accepts? <<-EOS
-    <script type="text/JavaScript">
-      #{code}
-    </script>
-      EOS
-    end
+    match { |code| parser_accepts? enclose_statements(code) }
   end
   Matchers.alias_matcher :be_valid, :be_a_statement
   Matchers.define_negated_matcher :be_invalid, :be_valid
   Matchers.alias_matcher :recognize, :be_valid
+
+  Matchers.define :output do |expected|
+    match { |code| parser_outputs? expected, given: enclose_statements(code) }
+  end
+
+  Matchers.define :typecheck do
+    match { |code| parser_typechecks? enclose_statements(code) }
+  end
+  Matchers.define_negated_matcher :fail_typecheck, :typecheck
+
 end
